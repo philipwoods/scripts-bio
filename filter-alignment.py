fad2274f48c3827eb1f1ab755d937dbc55efd399
Philip Woods <philipwoods93@gmail.com>
2022-09-30 15:34:04 -0700

    Start alignment analysis script

---
+++ filter-alignment.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import argparse
+from Bio import AlignIO
+
+def main(args):
+    print("Done")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    args = parser.parse_arguments()
+    main(args)
+
