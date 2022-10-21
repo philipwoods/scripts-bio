8351c2eb5318c63c393dc7b0ea334a29bd91c7c1
Philip Woods <philipwoods93@gmail.com>
2022-10-21 16:53:19 -0700

    Added basic options and list to alignment filter script

--- filter-alignment.py
+++ filter-alignment.py
@@ -6,10 +6,21 @@ import argparse
 from Bio import AlignIO
 
 def main(args):
+    alignment = AlignIO.read(args.fasta, "fasta")
+    if args.list:
+        for i, record in enumerate(alignment):
+            print("{index}\t{header}".format(index=i, header=record.id))
     print("Done")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    args = parser.parse_arguments()
+    parser.add_argument('fasta', metavar='FASTA', help="FASTA file containing the alignment of interest")
+    parser.add_argument('--list', '-l', action='store_true', help="Display indices of sequence headers in the FASTA")
+    parser.add_argument('--identity', '-i', type=float, default=1, help="Set the minimum identity threshold for conservation. Default: %(default)s")
+    args = parser.parse_args()
+    if not os.path.isfile(args.fasta):
+        sys.exit("Specified file does not exist: {}".format(args.fasta))
+    if (args.identity < 0) or (args.identity > 1):
+        sys.exit("Identity threshold must be a proportion between 0 and 1, inclusive.")
     main(args)
 
