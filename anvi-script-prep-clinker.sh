cbd5bcf0e41a32d5bfd7edba700dd45ed072aff5
Philip Woods <philipwoods93@gmail.com>
2023-04-10 16:56:18 -0700

    Create script to convert anvio locus DBs to clinker input

---
+++ anvi-script-prep-clinker.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+print_usage() {
+    echo "clinker-prep -i <in-dir> -o <out-dir>"
+}
+
+indir=""
+outdir=""
+
+while getopts ":he:o:" opt; do
+    case $opt in
+        h)  print_usage
+            exit 1
+            ;;
+        i)  indir="${OPTARG}"
+            ;;
+        o)  outdir=$(realpath ${OPTARG})
+            ;;
+        \?) echo "Invalid option: -${OPTARG}. Use the -h option for more information.">&2
+            exit 1
+            ;;
+        :)  echo "Option -$OPTARG requires an argument. Use the -h option for more information.">&2
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -z "$indir" ]]; then
+    echo "You must provide an input directory."
+    exit 1
+fi
+
+if [[ ! -d "$indir" ]]; then
+    echo "The provided directory does not exist: $indir"
+    exit 1
+fi
+
+if [[ ! $CONDA_DEFAULT_ENV =~ "anvio" ]]; then
+    echo "You need to activate an anvio conda environment before running this."
+    exit 1
+fi
+
+# Get real directory where script file is located,
+# even if the script was called via a symlink
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to  81 done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+# Extract GFF of annotations
+for f in ${indir}/*.db; do
+    anvi-get-sequences-for-gene-calls -c $f --export-gff3 -o ${f%.db}.gff
+done
+
+# Convert GFF to GB format
+for f in ${indir}/*.gff; do
+    gff_to_genbank $f ${f%.gff}.fa
+done
+
+# Copy GB files for clinker to the output directory
+cp ${indir}/*.gb $outdir
+
+echo "Done!"
+
