9f288b0df7cb49bbac7a5dc60cb8b51568c84dc8
Philip Woods <philipwoods93@gmail.com>
2023-04-10 16:55:10 -0700

    Add id-file input method

--- split-fasta.py
+++ split-fasta.py
@@ -5,6 +5,13 @@ import argparse
 from Bio import SeqIO
 
 def main(args):
+    if args.id_file is not None:
+        with open(args.id_file) as f:
+            file_ids = [line.strip() for line in f.readlines()]
+        if args.id is None:
+            args.id = file_ids
+        else:
+            args.id.extend(file_ids)
     for record in SeqIO.parse(args.fasta, "fasta"):
         # The record ID is anything before a space in the defline
         if (args.id is None) or (record.id in args.id):
@@ -18,8 +25,11 @@ if __name__ == "__main__":
     parser.add_argument("fasta", metavar="FASTA", help="A multi-sequence FASTA file.")
     parser.add_argument("-d", "--dir", default="", help="A directory to use for the output. Default: current directory")
     parser.add_argument("--id", nargs='+', help="A list of sequence IDs to output. Default: output everything")
+    parser.add_argument("--id-file", help="A file containing a list of sequence IDs, one per line, to output")
     args = parser.parse_args()
     if not os.path.isfile(args.fasta):
         sys.exit("The specified file does not exist: {}".format(args.fasta))
+    if args.id_file is not None and not os.path.isfile(args.id_file):
+        sys.exit("The specified file does not exist: {}".format(args.in_file))
     main(args)
 
