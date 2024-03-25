ea1554ad5095c331f16523d4e7d103fcadea3ba6
Philip Woods <philipwoods93@gmail.com>
2024-03-25 14:26:14 -0700

    Add script to sort fastas by sequence id

---
+++ sort-fasta.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import sys
+import os.path
+import argparse
+from Bio import SeqIO
+
+def main(args):
+    records = SeqIO.parse(args.fasta, "fasta"):
+    # The record ID is anything before a space in the defline
+    ids = [record.id for record in records]
+    ids.sort()
+    for seq_id in ids:
+        for record in records:
+            if record.id == seq_id:
+                SeqIO.write(record, sys.stdout, "fasta")
+
+if __name__ == "__main__":
+    desc = "Takes in a multi-sequence FASTA file and sorts the sequences by sequence ID."
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("fasta", metavar="FASTA", help="A multi-sequence FASTA file.")
+    args = parser.parse_args()
+    if not os.path.isfile(args.fasta):
+        sys.exit("The specified file does not exist: {}".format(args.fasta))
+    main(args)
+
