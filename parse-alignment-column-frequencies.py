cb27f641c8b2ac64fbd20646b9b982e9a171b371
Philip Woods <philipwoods93@gmail.com>
2024-06-26 15:08:12 -0700

    Add output framework and build subset group alignments

--- parse-alignment-column-frequencies.py
+++ parse-alignment-column-frequencies.py
@@ -3,7 +3,9 @@
 import os
 import sys
 from Bio import AlignIO
+from Bio.Align import MultipleSeqAlignment
 import pandas as pd
+from collections import defaultdict
 
 def main():
     help_string = """
@@ -17,7 +19,7 @@ def main():
         that script to work, both scripts must be in the same directory.
     """
     version_string = """
-    Last updated 4 November 2021
+    Last updated 19 June 2024
     """
 
     if "-h" in sys.argv or "--help" in sys.argv:
@@ -28,6 +30,41 @@ def main():
     output_dir = sys.argv[1]
     alignment_file= sys.argv[2]
     groups_file = sys.argv[3]
+
+    # Declare amino acid reference dictionary for output
+    amino_acids = {
+        'key': ['Alanine', 'Cysteine', 'Aspartate', 'Glutamate', 'Phenylalanine',
+                'Glycine', 'Histidine', 'Isoleucine', 'Lysine', 'Leucine',
+                'Methionine', 'Asparagine', 'Proline', 'Glutamine', 'Arginine',
+                'Serine', 'Threonine', 'Valine', 'Tryptophan', 'Tyrosine', 'Gap'],
+        'accession': ['A', 'C', 'D', 'E', 'F',
+                      'G', 'H', 'I', 'K', 'L',
+                      'M', 'N', 'P', 'Q', 'R',
+                      'S', 'T', 'V', 'W', 'Y', '-']
+    }
+
+    # Read in the alignment and sort by the ID field
+    with open(alignment_file, 'r') as f:
+        alignment = AlignIO.read(f, 'fasta')
+    # Read in groups file, columns 'samples' and 'group'
+    groups_db = pd.read_csv(groups_file, sep='\t', index_col='samples')
+    # Get a dictionary of the form {'G1': [seqid0, seqid2,...], 'G2': [seqid1, seqid3,...]}
+    groups = groups_db.groupby('group').groups
+
+    # create subset alignments of each group
+    group_seqs = defaultdict(list)
+    group_alns = {}
+    for seqrecord in alignment:
+        for group in groups.keys():
+            if seqrecord.id in groups[group]:
+                group_seqs[group].append(seqrecord)
+    print(group_seqs)
+    for group in groups.keys():
+        group_alns[group] = MultipleSeqAlignment(group_seqs[group])
+        print(group_alns[group])
+
+    out_df = pd.DataFrame(data=amino_acids)
+    out_df['function'] = "NA"
 
 if __name__ == "__main__":
     main()
