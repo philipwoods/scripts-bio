f8a14273afdc38f3063e47577b4917d5d24c9601
Philip Woods <philipwoods93@gmail.com>
2024-10-16 12:21:35 -0700

    Add length condition options

--- filter-fasta.py
+++ filter-fasta.py
@@ -4,6 +4,9 @@ import os.path
 import argparse
 from Bio import SeqIO
 
+def within_length_bounds(record, args):
+    return (len(record) > args.min) and (len(record) < args.max)
+
 def main(args):
     if args.list:
         for record in SeqIO.parse(args.fasta, "fasta"):
@@ -19,19 +22,21 @@ def main(args):
     output_records = []
     for record in SeqIO.parse(args.fasta, "fasta"):
         # The record ID is anything before a space in the defline
-        if (args.id is None) or (record.id in args.id):
+        if ((args.id is None) or (record.id in args.id)) and within_length_bounds(record, args):
             output_records.append(record)
-    SeqIO.write(output_records, args.output, "fasta")
+    SeqIO.write(output_records, args.out, "fasta")
 
 if __name__ == "__main__":
     desc = ("Takes in a multi-sequence FASTA file and searches it for the provided sequence IDs "
-            "or regular expression pattern against the sequence IDs. ")
+            "and/or by the provided minimum and maximum sequence lengths.")
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument("fasta", metavar="FASTA", help="A multi-sequence FASTA file.")
     parser.add_argument("--out", default=sys.stdout, help="A path to output the results. Default: stdout")
     parser.add_argument("--id", nargs='+', help="A list of sequence IDs to output. Default: output everything")
     parser.add_argument("--id-file", help="A file containing a list of sequence IDs, one per line, to output.")
     parser.add_argument("--list", "-l", action='store_true', help="List the available sequence IDs from the input.")
+    parser.add_argument("--min", type=int, default=0, help="Minimum acceptable sequence length to output.")
+    parser.add_argument("--max", type=int, default=100000000000000000, help="Maximum acceptable sequence length to output.")
     args = parser.parse_args()
     if not os.path.isfile(args.fasta):
         sys.exit("The specified file does not exist: {}".format(args.fasta))
