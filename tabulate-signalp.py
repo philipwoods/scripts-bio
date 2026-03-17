19b2755c01258ba7c6dd61cca6ee94c09a288daa
Philip Woods <philipwoods93@gmail.com>
2026-03-17 16:03:42 -0700

    Finish main logic

--- tabulate-signalp.py
+++ tabulate-signalp.py
@@ -7,26 +7,38 @@ import pandas as pd
 from collections import defaultdict
 from collections import namedtuple
 
-def main(args)
-    return
-
+def main(args):
+    # Load JSON input
+    with open(args.input, 'r') as f:
+        results = json.load(f)
+    data_dict = results['SEQUENCES']
+    out_dict = []
+    for entry in data:
+        seqname = entry['Name']
+        prediction = entry['Prediction']
+        prot_types = entry['Protein_types']
+        likelihoods = entry['Likelihood']
+        row = {
+            'seq_id': seqname,
+            'prediction': prediction
+        }
+        for name, p in zip(prot_types, likelihoods):
+            row[name] = p
+        output.append(row)
+    out_df = pd.DataFrame.from_records(out_dict)
+    out_df.to_csv(args.output, index=False, sep=args.sep)
 
 if __name__ == "__main__":
-    desc = ("A command-line utility to view and navigate downloaded BLAST results files. "
-            "Your BLAST results must be downloaded in Single-File JSON format from the NCBI results page."
-            "Requires pandas to run.")
-    epil = ("Note that if a report title contains spaces, you will need to put it in quotes 'like this' when "
-            "passing arguments to the --report option.")
-    parser = argparse.ArgumentParser(description=desc, epilog=epil)
-    parser.add_argument("-i", "--input", required=True, help="The Single-File JSON output from NCBI BLAST.")
-    parser.add_argument("-r", "--report", nargs='*', help="Select one or more reports to view in detail. To view all reports, use this option with no arguments.")
-    parser.add_argument("--hit", nargs='+', help="Specify the index of one or more hits to display per report. By default, all hits are displayed.")
-    parser.add_argument("-a", "--alignments", action='store_true', help="Display alignment reports instead of a summary table.")
-    parser.add_argument("-p", "--parameters", action='store_true', help="Display BLAST input parameters in the header of the results.")
-    parser.add_argument("-t", "--taxid", action='store_true', help="Display NCBI taxid in the results summary table.")
-    parser.add_argument("-l", "--align-length", action='store_true', help="Display alignment length in the results summary table.")
+    desc = ("A command-line utility to convert the standard output from SignalP from JSON to "
+            "a tabular format.")
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("input", help="The JSON output from SignalP 6.0")
+    parser.add_argument("--sep", default='\t', help="The separator to use in the output table. Default: TAB")
+    parser.add_argument("--output", default=sys.stdout, help="File name to write output. Default: STDOUT")
     args = parser.parse_args()
     if not os.path.isfile(args.input):
         sys.exit("Specified file does not exist: {}".format(args.input))
+    if len(args.sep) > 1:
+        sys.exit("Separator must be exactly one character long.")
     main(args)
 
